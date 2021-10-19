a69384bcb0eed240f8f884f7bb2efd9477d4f85a
Dam A <cgasconl@iesch.org>
2021-10-19 18:11:54 +0200

    19/10/2021

---
+++ 1Trimestre/6-DashBoard/app/src/main/java/com/carlos/a6_dashboard/Activity/Restaurantes_Activity.java
@@ -0,0 +1,16 @@
+package com.carlos.a6_dashboard.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.carlos.a6_dashboard.R;
+
+public class Restaurantes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurantes);
+    }
+}
