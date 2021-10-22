663485651b8a3a256a791332a7f535870d7b75ab
Dam A <cgasconl@iesch.org>
2021-10-22 15:41:53 +0200

    dadad

---
+++ 1Trimestre/7SplasApp/app/src/main/java/com/carlos/a7_splasapp/SplasActivity2.java
@@ -0,0 +1,14 @@
+package com.carlos.a7_splasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplasActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splas2);
+    }
+}
