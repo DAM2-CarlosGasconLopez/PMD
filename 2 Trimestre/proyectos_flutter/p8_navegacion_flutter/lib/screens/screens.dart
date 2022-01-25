be883980cdc8ea206b2867be3952298cfbeb9243
cgasconl <cgasconl@iesch.org>
2022-01-25 12:32:51 +0100

    fvrvf

---
+++ 2 Trimestre/proyectos_flutter/p8_navegacion_flutter/lib/screens/screens.dart
@@ -0,0 +1,6 @@
+
+export 'package:p8_navegacion_flutter/screens/pantalla_1Screen.dart';
+export 'package:p8_navegacion_flutter/screens/pantalla_2Screen.dart';
+export 'package:p8_navegacion_flutter/screens/pantalla_3Screen.dart';
+export 'package:p8_navegacion_flutter/screens/pantalla_4Screen.dart';
+export 'package:p8_navegacion_flutter/screens/pantalla_5Screen.dart';
